dc520c7c211272824b9447b265a79f87aa1b7670
luicast <luycast@hotmail.com>
2022-01-21 17:32:00 -0500

    organizando codigo

--- assets/index.js
+++ assets/index.js
@@ -2,10 +2,10 @@ import MediaPlayer from "./MediaPlayer.js"
 import AutoPlay from "./plugins/AutoPlay.js"
 
 const video = document.querySelector("video")
-const player = new MediaPlayer({ el:video, Plugins:[new AutoPlay()] })
-
 const playbutton = document.querySelector("#playPause")
-playbutton.onclick = () => player.togglePlay()
-
 const mutebutton = document.querySelector("#muteUnmute")
+
+const player = new MediaPlayer({el: video, Plugins:[new AutoPlay()] })
+
+playbutton.onclick = () => player.togglePlay()
 mutebutton.onclick = () => player.togglemute()
--- assets/plugins/AutoPlay.js
+++ assets/plugins/AutoPlay.js
@@ -1,4 +1,3 @@
-
 function AutoPlay(){}
 
 AutoPlay.prototype.run = function(Player){
