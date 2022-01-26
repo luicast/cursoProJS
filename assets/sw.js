2e8315d40526b5ff45e2885400d6cf76c66e2c3f
luicast <luycast@hotmail.com>
2022-01-26 15:06:43 -0500

    termiando la configuracion

---
+++ assets/sw.js
@@ -0,0 +1,45 @@
+const VERSION = 'v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(precache());
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request; 
+  // Only GET
+  if (request.method !== 'GET') {
+    return;
+  }
+
+  // buscamos en cache
+  event.respondWith(cachedResponse(request));
+
+  // actualizamos el cache
+  event.waitUntil(updateCache(request));
+});
+
+async function precache() {
+  const cache = await caches.open(VERSION);
+  return cache.addAll([
+    "./",
+    "./index.html",
+    "./assets/index.js",
+    "./assets/MediaPlayer.js",
+    "./assets/plugins/AutoPlay.js",
+    "./assets/plugins/AutoPause.js",
+    "./assets/index.css",
+    "./assets/kickthecan2.mkv",
+  ]);
+}
+
+async function cachedResponse(request) {
+  const cache = await caches.open(VERSION);
+  const response = await cache.match(request);
+  return response || fetch(request);
+}
+
+async function updateCache(request) {
+  const cache = await caches.open(VERSION);
+  const response = await fetch(request);
+  return cache.put(request, response);
+}
